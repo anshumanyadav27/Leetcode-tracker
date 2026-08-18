29797c5eda6909579f788488a42a4d9731405383
anshumanyadav27 <anshuman27yadav11@gmail.com>
2026-08-18 23:13:14 +0530

    Time: 8 ms (42.98%), Space: 15.3 MB (28.45%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+
+        long long num = 0;
+        char op = '+';
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (isdigit(s[i])) {
+                num = num * 10 + (s[i] - '0');
+            }
+            if ((!isdigit(s[i]) && s[i] != ' ') || i == s.length() - 1) {
+
+                if (op == '+') {
+                    st.push(num);
+                }
+                else if (op == '-') {
+                    st.push(-num);
+                }
+                else if (op == '*') {
+                    int x = st.top();
+                    st.pop();
+                    st.push(x * num);
+                }
+                else if (op == '/') {
+                    int x = st.top();
+                    st.pop();
+                    st.push(x / num);
+                }
+
+                op = s[i];
+                num = 0;
+            }
+        }
+
+        int ans = 0;
+
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
